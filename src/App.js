f44e6e7ccb5440e4e1908bb1eb235989c51740ed
Your Name <goncharovigor12@gmail.com>
2022-09-22 20:09:08 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import './index.scss';
+import { useEffect } from "react";
+import { useState } from "react";
+import "./index.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(Number(localStorage.getItem("count")));
+  }, []);
+
+  const save = () => {
+    localStorage.setItem("count", `${count}`);
+  };
+
+  const increment = () => {
+    setCount(count + 1);
+    save();
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+    save();
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={decrement} className="minus">
+          - Минус
+        </button>
+        <button onClick={increment} className="plus">
+          Плюс +
+        </button>
       </div>
     </div>
   );
